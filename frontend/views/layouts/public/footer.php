56b71f2056f4e82868b2b56511bfa874f7d75357
Cat <294587853@qq.com>
2021-01-09 07:33:17 +0800

    update

--- frontend/views/layouts/public/footer.php
+++ frontend/views/layouts/public/footer.php
@@ -1,55 +1,32 @@
-<!-- BEGIN PRE-FOOTER -->
-<div class="page-prefooter">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
-                <h2>关于我们</h2>
-                <p> 作为一个yii2爱好者，加入我们来完善这个应用吧。复制下面网址到浏览器。github.com/e282486518/yii2admin </p>
-            </div>
-            <div class="col-md-3 col-sm-6 col-xs12 footer-block">
-                <h2>订阅邮件</h2>
-                <div class="subscribe-form">
-                    <form action="javascript:;">
-                        <div class="input-group">
-                            <input type="text" placeholder="phphome@qq.com" class="form-control">
-                            <span class="input-group-btn">
-                                <button class="btn" type="submit">提交</button>
-                            </span>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
-                <h2>加入我们</h2>
-                <ul class="social-icons">
-                    <li><a href="javascript:;" data-original-title="rss" class="rss"></a></li>
-                    <li><a href="javascript:;" data-original-title="facebook" class="facebook"></a></li>
-                    <li><a href="javascript:;" data-original-title="twitter" class="twitter"></a></li>
-                    <li><a href="javascript:;" data-original-title="googleplus" class="googleplus"></a></li>
-                    <li><a href="javascript:;" data-original-title="linkedin" class="linkedin"></a></li>
-                    <li><a href="javascript:;" data-original-title="youtube" class="youtube"></a></li>
-                    <li><a href="javascript:;" data-original-title="vimeo" class="vimeo"></a></li>
-                </ul>
-            </div>
-            <div class="col-md-3 col-sm-6 col-xs-12 footer-block">
-                <h2>联系方式</h2>
-                <address class="margin-bottom-40"> Phone: 136 xxxx 1235
-                    <br> Email:
-                    <a href="mailto:phphome@qq.com">phphome@qq.com</a>
-                </address>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- END PRE-FOOTER -->
-<!-- BEGIN INNER FOOTER -->
-<div class="page-footer">
-    <div class="container"> 
-        2016 &copy; Metronic by huanglongfei.cn.
-        <a href="https://github.com/e282486518/yii2admin" title="黄龙飞" target="_blank">查看应用!</a>
-    </div>
-</div>
-<div class="scroll-to-top">
-    <i class="icon-arrow-up"></i>
-</div>
-<!-- END INNER FOOTER -->
+	<footer id="footer">
+		<div class="footer-container">
+			<div class="footer-upper">
+				<div class="footer-link">
+					<a href="/"><span class="footer_logo cn"><img
+							src="https://img.hxcsxs1.xyz/upload/site/20200526-1/ac9236d845a001dc464fd808df9d350b.png"
+							width="100%"></span></a>
+				</div>
+			</div>
+			<div class="footer-declaration">
+				<a
+					href="https://wpa.qq.com/msgrd?v=3&uin=766632318&site=qq&menu=yes"
+					target="_blank">聯絡我們</a> <span>Copyright © 2019
+					含羞草实习所www.hxcsxs.org All Rights Reserved.</span> <span>禁止中國大陸用戶和未滿18周歲用戶訪問</span>
+				<span>
+				<script>
+                    var _hmt = _hmt || [];
+                    (function() {
+                      var hm = document.createElement("script");
+                      hm.src = "https://hm.baidu.com/hm.js?ef7d69b0d8afbcf82c7fd9048fc1d037";
+                      var s = document.getElementsByTagName("script")[0]; 
+                      s.parentNode.insertBefore(hm, s);
+                    })();
+                </script> 
+                </span>
+			</div>
+		</div>
+	</footer>
+	<script src="/template/default_pc/style/js/jquery.min.js"></script>
+	<script src="/template/default_pc/style/js/bootstrap.min.js"></script>
+	
+	<div class="layui-layer-move"></div>
---
+++ frontend/views/layouts/public/header.php
@@ -0,0 +1,63 @@
+<div class="header-container">
+			<div class="logo_switch">
+				<a href="/" class="header_link">
+					<span class="header_logo cn">
+						<img src="/static/images/logo_cn.png" width="100%">
+					</span>
+				</a>
+			</div>
+			<div class="website_switch">
+				<a id="userLevelIconM" href="/index.php/user/index.html"
+					class="menusite_l" style="display: inline;"><span
+					class="icon hd-member cn"></span></a>
+			</div>
+			<div class="searchbar_wrapper">
+				<form method="get" name="search" action="/index.php/vod/search.html">
+					<input type="text" name="wd" placeholder="搜洞洞" required="required"
+						class="search_bar" />
+					<div class="search_btn">
+						<button style="background: transparent;" type="submit">
+							<i class="fa fa-search" style="line-height: 30px; color: #fff;"></i>
+						</button>
+					</div>
+				</form>
+			</div>
+			<div class="searchkey_wrapper">
+				<span>熱門&nbsp;:</span> <a
+					href="/index.php/vod/search/wd/%E4%BA%AC%E9%A6%99.html"
+					target="_blank">京香</a> <a
+					href="/index.php/vod/search/wd/%E6%B3%A2%E5%A4%9A%E9%87%8E%E7%B5%90%E8%A1%A3.html"
+					target="_blank">波多野結衣</a> <a
+					href="/index.php/vod/search/wd/%E9%A2%9C%E5%80%BC.html"
+					target="_blank">颜值</a> <a
+					href="/index.php/vod/search/wd/%E5%88%B6%E6%9C%8D.html"
+					target="_blank">制服</a> <a
+					href="/index.php/vod/search/wd/%E5%B7%A8%E4%B9%B3.html"
+					target="_blank">巨乳</a> <a
+					href="/index.php/vod/search/wd/%E5%8F%A3%E4%BA%A4.html"
+					target="_blank">口交</a> <a href="/index.php/vod/search/wd/SM.html"
+					target="_blank">SM</a> <a
+					href="/index.php/vod/search/wd/%E4%BA%BA%E5%A6%BB.html"
+					target="_blank">人妻</a>
+			</div>
+			<div class="phone_down">
+				<a href="/home.html" class="down-button"><span
+					style="color: rgb(178, 58, 238);"><i class="fa fa-
+
+download"></i>&nbsp;發布地址</span></a>
+			</div>
+			<div id="userLevelIcon" class="login_menu" style="">
+				<div class="member_group">
+					<span class="user_profile"><span
+						class="sprite sprite-icon-silver user_pic_wrapper"></span> <span
+						class="user_id silver">默认会员</span></span>
+					<ul class="member_dropdown">
+						<a href="/index.php/user/index.html" target="_blank"
+							class="list_style"><li>會員中心</li></a>
+						<a href="/index.php/user/favs.html" target="_blank"
+							class="list_style"><li>我的收藏</li></a>
+						<a href="/index.php/user/logout.html" class="list_style"><li>退出</li></a>
+					</ul>
+				</div>
+			</div>
+		</div>
