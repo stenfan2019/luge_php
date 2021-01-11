20eb7d9ca2b4de9f9031f1b34cf3460b6d1aa6f9
Cat <294587853@qq.com>
2021-01-12 07:24:28 +0800

    update

--- admin/controllers/SpvideoController.php
+++ admin/controllers/SpvideoController.php
@@ -10,6 +10,28 @@ class SpvideoController extends Base
 {
     public function actionList()
     {
-    
+        $data = Yii::$app->request->get('data');
+        if(1 == $data){
+            $page = Yii::$app->request->get('page',1);
+            $limit = Yii::$app->request->get('limit',20);
+            $query = SpVideo::find();
+            $countQuery = clone $query;
+            $count = $countQuery->count();
+            $page_num = ceil($count / $limit);
+            $pages = new Pagination(['totalCount' => $count,'defaultPageSize'=>$limit,'page'=>$page-1]);
+            $models = $query->offset($pages->offset)
+                    ->orderBy('id desc')
+                    ->limit($limit)
+                    ->asArray()->all();
+            $data = [
+                'data'  => SpVideo::fetchTitle($models, SpVideo::$field_title,2),
+                'page'  => $page,
+                'count' => $count,
+                'limit' => $limit
+            ];
+            $this->success($data);
+        }else{
+            return $this->render('list');
+        }
     }
 }
