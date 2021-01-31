282b31fe391cc0d724cd1059f63e99977a165eeb
stenfan <stenfan2019@gmail.com>
2021-01-31 15:40:08 +0800

    搜索

--- frontend/views/layouts/public/search.php
+++ frontend/views/layouts/public/search.php
@@ -1,5 +1,5 @@
   <div class="con_search">
-    <form method="get" name="search" action="/search" class="btn_search focus">
+    <form method="get" name="search" action="/video/search" class="btn_search focus">
      <span class="tit_search">搜洞洞</span> 
      <input type="text" name="wd" placeholder="搜洞洞" required="required" class="search_bar_m"> 
      <div class="search_btn_m">
